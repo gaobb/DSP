5cad8487e891ff26473f967340e6edee9179575e
gaobb <gaobb@lamda.nju.edu.cn>
2017-04-15 11:20:50 +0800

    add compiling in setup.m

--- setup.m
+++ setup.m
@@ -1,12 +1,66 @@
-%  Add dependency toolbox to matlab work path 
+function setup(doCompile, matconvnetOpts)
+% SETUP  Setup paths, external, etc.
+% 
+%   doCompile:: false
+%       Set to true to compile the libraries
+%   matconvnetOpts:: struct('enableGpu',false)
+%       Options for vl_compilenn
 
-fprintf('# add matconvernet, vlfeat, liblinear to matlab work path\n');
-%vlfeat
-run(fullfile('/home/gaobb/mywork/toolbox/vlfeat-0.9.19/toolbox/vl_setup.m')) ;          
- %liblinear
-addpath(genpath(fullfile('/home/gaobb/mywork/toolbox/liblinear-2.1_dense'))) ;                                  
-%matconvnet
-run(fullfile('/home/gaobb/mywork/toolbox/matconvnet-1.0-beta24/matlab/vl_setupnn.m')) ; 
+if nargin==0, 
+    doCompile = false;
+elseif nargin<2, 
+    matconvnetOpts = struct('enableGpu', false); 
+end
 
+if doCompile && gpuDeviceCount()==0 ...
+    && isfield(matconvnetOpts,'enableGpu') && matconvnetOpts.enableGpu, 
+    fprintf('No supported gpu detected! ');
+    return;
+end
+
+% -------------------------------------------------------------------------
+%                                                                   vlfeat
+% -------------------------------------------------------------------------
+if doCompile,
+    cmd = 'make -C external/vlfeat/ clean';
+    if system(cmd), 
+        error('Error while excution: %s', cmd);
+    end
+    cmd = sprintf('make -C external/vlfeat/ MEX=%s', ...
+        fullfile(matlabroot,'bin','mex'));
+    if system(cmd), 
+        error('Error while excution: %s', cmd);
+    end
+end
+run external/vlfeat/toolbox/vl_setup.m
+
+% -------------------------------------------------------------------------
+%                                                               matconvnet
+% -------------------------------------------------------------------------
+if doCompile, 
+    run external/matconvnet/matlab/vl_setupnn.m
+    cd external/matconvnet
+    vl_compilenn(matconvnetOpts);
+    cd ../..
+end
+run external/matconvnet/matlab/vl_setupnn.m
+
+
+% -------------------------------------------------------------------------
+%                                                             DenseLibLinear
+% -------------------------------------------------------------------------
+if doCompile, 
+    run external/DenseLibLinear/matlab/vl_setupnn.m
+    cd external/DenseLibLinear
+    make clean
+    make
+    cp train_dense predict_dense ../../
+    cd ../..
+end
+% -------------------------------------------------------------------------
+%                                                      add utils setup_imdb
+% -------------------------------------------------------------------------
 addpath(genpath('./utils'))
 addpath(genpath('./setup_imdb'))
+
+
